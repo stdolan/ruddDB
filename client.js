312b8419c893d6fe4e80400d4c377bb42a714bf5
Sean T Dolan <stdolan@gmail.com>
2015-04-11 20:23:07 -0700

    Tabs to spaces, command-line specification of client connection.

--- client.js
+++ client.js
@@ -1,7 +1,8 @@
 var http = require("http");
+var url = process.argv[2] || "http://localhost";
 
-http.get("http://localhost", function(res) {
- 	console.log("Got response: " + res.statusCode);
+http.get(url, function(res) {
+    console.log("Got response: " + res.statusCode);
 }).on('error', function(e) {
- 	console.log("Got error: " + e.message);
+    console.log("Got error: " + e.message);
 });
--- server.js
+++ server.js
@@ -2,10 +2,10 @@ var http = require("http");
 var port = 80;
 
 var server = http.createServer(function (req, res) {
-	res.writeHead(200, {
-		'Content-Length': 0,
-  		'Content-Type': 'text/plain' });
-	res.end();
+    res.writeHead(200, {
+        'Content-Length': 0,
+        'Content-Type': 'text/plain' });
+    res.end();
 });
 
 server.listen(port);
