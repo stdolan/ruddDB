28b34d1f8bbd4c67e6f95deef3e74aa2fbf29a70
Sean T Dolan <stdolan@gmail.com>
2015-04-15 15:41:23 -0700

    Added util.js, and an array comparison function.

---
+++ util.js
@@ -0,0 +1,24 @@
+/*
+ * array_compare takes two arrays, a and b, and recursively checks to see if
+ * each element in a and b fits some compare function (which defaults to
+ * type-strict equivalence).
+ */
+function _array_compare(a, b, compare_func) {
+    if (a.length != b.length)
+        return false;
+
+    compare_func = compare_func || function(x, y) { return x === y; };
+
+    for (var i = 0; i < a.length; i++)
+        if (a[i].constructor === Array) {
+            if (!_array_compare(a[i], b[i], compare_func))
+                return false;
+        }
+
+        else if (!compare_func(a[i], b[i]))
+            return false;
+
+    return true;
+}
+
+exports.array_compare = _array_compare;
