f423c7f1c44741b345accd6162f3e82fab4b7a5b
Sean T Dolan <stdolan@gmail.com>
2015-04-16 16:40:52 -0700

    Client should work now.

--- client.js
+++ client.js
@@ -1,13 +1,18 @@
 var http = require("http");
-var os = require("os");
+var rl = require("readline");
 var url = process.argv[2] || "http://localhost:8090";
 
 process.stdin.setEncoding('utf8');
 
+var prompt = rl.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
 http.get(url, function(res) {
     if (res.statusCode === 200) {
         console.log("Connected to " + url + ".");
-        repl();
+        prompt.question("> ", get_input);
     }
     else
         console.log("Got code " + res.statusCode + ".");
@@ -15,21 +20,54 @@ http.get(url, function(res) {
     console.log("Got error: " + e.message);
 });
 
-function repl() {
-    process.stdout.write("> ");
+// Matches ["http"/"https", hostname, path, port].
+var url_parser_regex = /(http|https):\/\/([^:\/]*)([^:]*)?:(\d*)/;
+var parsed_url = url.match(url_parser_regex);
 
-    process.stdin.on('readable', function() {
-        var chunk = process.stdin.read();
+if (parsed_url === null) {
+    console.log("Could not parse " + url + "!");
+}
 
-        if (chunk === null)
-            return;
+var post_options = {
+    host: parsed_url[2],
+    path: parsed_url[3] || "",
+    port: parsed_url[4] || 80,
+    method: 'POST',
+    headers: {
+        'Content-Type': "text/javascript",
+        'Content-Length': 0
+    }
+};
 
-        if (chunk === "quit" + os.EOL
-            || chunk === "exit" + os.EOL
-            || chunk === "q" + os.EOL)
-            process.exit();
+function send_command(command) {
+    post_options.headers["Content-Length"] = command.length;
 
-        process.stdout.write('data: ' + chunk);
-        process.stdout.write("> ");
+    var post_req = http.request(post_options, function(res) {
+        res.setEncoding("utf8");
+        res.on("data", function (chunk) {
+            if (chunk[chunk.length - 1] === "\n")
+                prompt.write("Received:" + chunk);
+            else
+                prompt.write("Received: " + chunk + "\n");
+        });
+        res.on("end", function() {
+            prompt.question("> ", get_input);
+        });
     });
+
+    post_req.write(command);
+    post_req.end();
+}
+
+function get_input(str) {
+    if (str === null)
+        return;
+
+    if (str === "quit"
+        || str === "exit")
+        process.exit();
+
+    prompt.write("Sending: " + str + "\n");
+    send_command(str);
+    prompt.pause();
 }
